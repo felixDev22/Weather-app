19fea9cd0f34d95f13b05a28d889d0033fd5bdfd
felixDev22 <felixnzomo@gmail.com>
2023-02-14 00:00:40 +0300

    Add images, Create home page structure

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import './Homepage.css';
 
-const Homepage = () => <div>Homepage</div>;
+const Homepage = () => (
+  <div className="hero">
+    <div className="select-region">
+      <p>Select Region</p>
+      <select>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Africa">Africa</option>
+        <option value="North America">North America</option>
+        <option value="South America">South America</option>
+        <option value="Antarctic">Antarctic</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </div>
+    <div className="row">
+      <div className="title">
+        <h2>Region</h2>
+        <span>countries</span>
+        <div className="hero-back" />
+      </div>
+    </div>
+
+    <section className="intro">
+      <h5>STATS BY COUNTRY</h5>
+      <div className="states">
+        <div className="card">
+          <h2>Country</h2>
+          <p>Capital</p>
+        </div>
+        <div className="card">
+          <h2>Country</h2>
+          <p>Capital</p>
+        </div>
+        <div className="card">
+          <h2>Country</h2>
+          <p>Capital</p>
+        </div>
+        <div className="card">
+          <h2>Country</h2>
+          <p>Capital</p>
+        </div>
+      </div>
+    </section>
+  </div>
+);
 
 export default Homepage;
