208589eff06e90b4d39db8a4e53c8f14582e35c4
felixDev22 <felixnzomo@gmail.com>
2023-02-13 22:13:43 +0300

    add global settings, add countryApi & weatherApi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
         <h1>Weather App</h1>
       </header>
