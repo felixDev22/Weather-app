22a2ae646764fb202ce83c3536392038ff164902
felixDev22 <felixnzomo@gmail.com>
2023-02-14 03:13:36 +0300

    Update weather Api, use lat and lon

--- src/redux/weatherApi.js
+++ src/redux/weatherApi.js
@@ -3,23 +3,29 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 const Url = 'https://api.openweathermap.org/data/2.5/weather';
 const apiKey = 'a04ae402db5b7b18b3013ff97a878d15';
 
-export const getWeather = createAsyncThunk('weather/fetch', async () => {
-  const response = await fetch(
-    `${Url}?lat=0.0236&lon=37.9062&appid=${apiKey}&units=metric`,
-  );
+export const getWeather = createAsyncThunk(
+  'weather/fetch',
+  async ({ lat, lon }) => {
+    const response = await fetch(
+      `${Url}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`,
+    );
 
-  const result = await response.json();
-  const weather = {
-    id: result.id,
-    name: result.name,
-    description: result.weather[0].description,
-    humidity: result.main.humidity,
-    pressure: result.main.pressure,
-    temp: result.main.temp,
-    wind: result.wind.speed,
-  };
-  return weather;
-});
+    const result = await response.json();
+    const weather = {
+      id: result.id,
+      name: result.name,
+      description: result.weather[0].description,
+      humidity: result.main.humidity,
+      pressure: result.main.pressure,
+      temp: result.main.temp,
+      wind: result.wind.speed,
+      visibility: result.visibility,
+      temp_min: result.main.temp_min,
+      temp_max: result.main.temp_max,
+    };
+    return weather;
+  },
+);
 
 const initialState = {
   weather: {},
