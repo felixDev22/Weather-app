b55c2f0303e1e8d9a951d40cc9bdf9c5922252f6
felixDev22 <felixnzomo@gmail.com>
2023-02-14 14:08:36 +0300

    Add styling for home and detail page

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getWeather } from '../../redux/weatherApi';
 
 import './Details.css';
@@ -19,8 +19,16 @@ const Details = () => {
     }
   }, [dispatch, country]);
 
+  let weatherDetails = (
+    <div className="no-data">
+      <h3>No data found!!</h3>
+      <p>Please go back to</p>
+      <Link to="/">Home</Link>
+    </div>
+  );
+
   if (country && weather.name) {
-    return (
+    weatherDetails = (
       <>
         <div className="details-hero">
           <div className="title">
@@ -29,60 +37,48 @@ const Details = () => {
             <p>Current weather</p>
           </div>
         </div>
-        <div>
-          {weather.loading && <h1>Loading...</h1>}
-          {!weather.loading && weather.error ? (
-            <div>
-              Error:
-              {weather.error}
+        <div className="weather-info">
+          <div className="info">
+            <div className="d-card">
+              <h4 className="title">Humidity</h4>
+              <h3>{weather.humidity}</h3>
             </div>
-          ) : null}
-          {!weather.loading && weather.weather ? (
-            <div className="weather-info">
-              <div className="info">
-                <div className="d-card">
-                  <h4 className="title">Humidity</h4>
-                  <h3>{weather[0].humidity}</h3>
-                </div>
 
-                <div className="d-card">
-                  <h4 className="title">Pressure</h4>
-                  <h3>{weather[0].pressure}</h3>
-                </div>
-
-                <div className="d-card">
-                  <h4 className="title">Temp Min</h4>
-                  <h3>{weather[0].temp_min}</h3>
-                </div>
-
-                <div className="d-card">
-                  <h4 className="title">Temp Max</h4>
-                  <h3>{weather.temp_max}</h3>
-                </div>
-
-                <div className="d-card">
-                  <h4 className="title">Feels Like</h4>
-                  <h3>{weather[0].wind}</h3>
-                </div>
-
-                <div className="d-card">
-                  <h4 className="title">Wind</h4>
-                  <h3>{weather[0].wind}</h3>
-                </div>
-
-                <div className="d-card">
-                  <h4 className="title">Visibility</h4>
-                  <h3>{weather[0].visibility}</h3>
-                </div>
-              </div>
+            <div className="d-card">
+              <h4 className="title">Pressure</h4>
+              <h3>{weather.pressure}</h3>
             </div>
-          ) : null}
+
+            <div className="d-card">
+              <h4 className="title">Temp Min</h4>
+              <h3>{weather.temp_min}</h3>
+            </div>
+
+            <div className="d-card">
+              <h4 className="title">Temp Max</h4>
+              <h3>{weather.temp_max}</h3>
+            </div>
+
+            <div className="d-card">
+              <h4 className="title">Feels Like</h4>
+              <h3>{weather.wind}</h3>
+            </div>
+
+            <div className="d-card">
+              <h4 className="title">Wind</h4>
+              <h3>{weather.wind}</h3>
+            </div>
+
+            <div className="d-card">
+              <h4 className="title">Visibility</h4>
+              <h3>{weather.visibility}</h3>
+            </div>
+          </div>
         </div>
       </>
     );
   }
-
-  return <div className="container" />;
+  return <div className="container">{weatherDetails}</div>;
 };
 
 export default Details;
